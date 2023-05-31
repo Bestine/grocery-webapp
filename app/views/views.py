2ff7996c7b83984f90622d555def7bd77dd6403a
Bestine <bestineokinda@gmail.com>
2023-05-31 10:52:26 +0300

    Created login page

--- app/views/views.py
+++ app/views/views.py
@@ -5,4 +5,12 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 # Add more routes and views as needed
