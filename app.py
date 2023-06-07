6a6c7d43f93378ee063f62dfd7bed063b55d52b2
Bestine <bestineokinda@gmail.com>
2023-06-07 15:11:18 +0300

    Changed file location

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
-from flask import Flask
+# from flask import Flask
+
+# app = Flask(__name__)
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/login')
+def items():
+    return render_template('login.html')
+
+# Other routes and configurations...
+
+if __name__ == '__main__':
+    app.run()
